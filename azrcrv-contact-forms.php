52252978499f574c1290d4247478086653c73df6
azurecurve <github@azurecurve.co.uk>
2024-02-03 12:47:51 +0000

    Release v2.0.4

--- azrcrv-contact-forms.php
+++ azrcrv-contact-forms.php
@@ -1,14 +1,18 @@
 <?php
 /**
  * ------------------------------------------------------------------------------
- * Plugin Name: Contact Forms
- * Description: Create simple contact forms using the simple contact-form shortcode; supports multiple contact forms on a page.
- * Version: 2.0.3
- * Author: azurecurve
- * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
- * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/contact-forms/
- * Text Domain: azrcrv-cf
- * Domain Path: /assets/languages
+ * Plugin Name:		Contact Forms
+ * Description:		Create simple contact forms using the simple contact-form shortcode; supports multiple contact forms on a page.
+ * Version:			2.0.4
+ * Requires CP:		1.0
+ * Author:			azurecurve
+ * Author URI:		https://development.azurecurve.co.uk/classicpress-plugins/
+ * Plugin URI:		https://development.azurecurve.co.uk/classicpress-plugins/contact-forms/
+ * Donate link:		https://development.azurecurve.co.uk/support-development/
+ * Text Domain:		azrcrv-cf
+ * Domain Path:		/assets/languages
+ * License:			GPLv2 or later
+ * License URI:		http://www.gnu.org/licenses/gpl-2.0.html
  * ------------------------------------------------------------------------------
  * This is free software released under the terms of the General Public License,
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
